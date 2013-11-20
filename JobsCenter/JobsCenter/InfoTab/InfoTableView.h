ab6e9695c45dc423116c3cce41d8ad181f9a116d
hanson <hangzhong.yu@gmail.com>
2013-11-20 15:27:54 +0800

    adjust the ui

---
+++ JobsCenter/JobsCenter/InfoTab/InfoTableView.h
@@ -0,0 +1,13 @@
+//
+//  InfoTableView.h
+//  JobsCenter
+//
+//  Created by hanson on 11/20/13.
+//  Copyright (c) 2013 Jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoTableView : UITableView
+
+@end
